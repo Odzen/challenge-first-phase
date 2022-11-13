0cecf83ce7172defbbc5b04705d6e4368bf963e9
Odzen <jsebastian.va@gmail.com>
2022-11-13 11:39:21 -0500

    One decremented - DONE

--- 1_one_decremented/one_decremented.py
+++ 1_one_decremented/one_decremented.py
@@ -1,7 +1,13 @@
 def OneDecremented(strParam):
+  if len(strParam) < 1:
+    return
+  
+  counter = 0
+  
+  for index in range(0, len(strParam) - 1):
+    if int(strParam[index]) - int(strParam[index+1]) == 1:
+      counter+=1  
+      
+  return counter
 
-  # code goes here
-  return strParam
-
-# keep this function call here 
 print(OneDecremented(input()))
