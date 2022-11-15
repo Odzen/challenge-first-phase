94432685a3ba4fd3871109d4bf7bdfba40b524f0
Odzen <jsebastian.va@gmail.com>
2022-11-14 22:03:19 -0500

    Most free time - DONE

--- 3_most_free_time/most_free_time.py
+++ 3_most_free_time/most_free_time.py
@@ -1,7 +1,37 @@
+def turnToMinutes(time):
+  
+  formatedTime = time[:-2].split(":") # Take hours and minutes
+  
+  # Convert to military time
+  if time[-2:] == "AM":
+    formatedTime = [int(formatedTime[0]), int(formatedTime[1])]
+  else:
+    formatedTime = [int(formatedTime[0])+12, int(formatedTime[1])]
+  
+  if (formatedTime[0]==12 or formatedTime[0]==24):
+    formatedTime[0] = formatedTime[0]-12
+  
+  return formatedTime[0] * 60 + formatedTime[1]
+
 def MostFreeTime(strArr):
+  evalutedInput = eval(strArr)
+   
+  listTimeMinutes = []
+  
+  for time in evalutedInput:
+    listTimeMinutes.append([turnToMinutes(element) for element in time.split("-")])
+    
+  listTimeMinutes.sort()
+  
+  mostfreeTime =  0
+  for i in range(len(listTimeMinutes)-1):
+    freeTimeGap  = listTimeMinutes[i+1][0] - listTimeMinutes[i][1]
+    
+    # Check each gap of free time
+    if freeTimeGap > mostfreeTime:
+      mostfreeTime = freeTimeGap
+    
+  return "%02d:%02d" %(mostfreeTime/60, mostfreeTime) 
+  
 
-  # code goes here
-  return strArr
-
-# keep this function call here 
 print(MostFreeTime(input()))
